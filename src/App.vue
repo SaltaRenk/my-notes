82eaab958673dc086cd630eca234ddd91a9871e1
Danil Kutlyev <kutlyev_dz@netology.ru>
2023-12-04 17:15:20 +0500

    Test-app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="app">
+      <my-dialog :show="dialogVisible">
+      <post-form></post-form>
+      </my-dialog>
+      <header class="header">
+        <a href="#"><img src="@/components/UI/logo.svg" alt="Логотип сайта" class="logo"></a>
+        <button class="enter--button" @click="showDialog">Вход</button>
+      </header>
+      <body class="body">
+        <h1>Мои заметки</h1>
+        <p>Не забывай о важном, храни его в облаке.</p>
+      </body>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PostForm from '@/components/PostForm';
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    PostForm
+  },
+  data() {
+    return {
+      dialogVisible: false,
+    }
+  },
+  methods: {
+    showDialog() {
+      this.dialogVisible = true;
+    }
   }
 }
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  background-color: #0A1F38;
 }
+.body {
+  background-image: url('@/components/UI/Group.svg');
+  background-repeat: no-repeat;
+  position: fixed;
+  top: 100px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  z-index: 1;
+}
+.enter--button{
+  background-color: #B1C909;
+  font-size: 20px;
+  border-radius: 32px;
+  border: 0;
+  width: 144px;
+  height: 56px;
+  }
+  .enter--button::before{
+    content: "";
+    display: inline-block;
+    background:no-repeat url("@/components/UI/Vector.svg");
+    width: 32px;
+    height: 32px;
+  }
+  .enter--button:hover {
+    background-color: #B1C909;
+  }
 </style>
